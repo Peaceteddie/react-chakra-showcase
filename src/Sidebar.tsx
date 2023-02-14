65adef3afe000bc8b3a41ee7d085ad8e64a01671
tithxa <tithxa@syntronic.com>
2023-02-14 11:50:04 +0100

    Added linkedin to social media

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,11 +1,12 @@
 import { Box, Center, Icon, useColorModeValue } from "@chakra-ui/react";
 import { ReactComponent as GithubIcon } from "./assets/github-mark.svg";
 import { ReactComponent as GithubIconWhite } from "./assets/github-mark-white.svg";
+import { ReactComponent as LinkedInIcon } from "./assets/LinkedInIcon.svg";
 
 export default function Sidebar() {
   const size = [12, 14, 16];
-  const smaller = size.map((num) => num / 2);
 
+  const brightness = useColorModeValue(0, 200);
   const bgColor = useColorModeValue("gray.200", "gray.600");
   const GHIcon = useColorModeValue(GithubIcon, GithubIconWhite);
 
@@ -14,21 +15,31 @@ export default function Sidebar() {
       <Icon viewBox="0 0 50 50" color={bgColor} height={size} width={size}>
         <path d="M 0 0 C 0,50 50,25 50,50 H0Z" fill="currentColor" />
       </Icon>
-      <Center bgColor={bgColor} flexDirection={"column"} padding={smaller}>
+      <Center bgColor={bgColor} flexDirection={"column"}>
         <a
           href="https://www.github.com/peaceteddie/"
           rel="noreferrer"
           target="_blank"
           title="Link to my github"
         >
+          <Icon viewBox="0 0 98 96" height={size} px={2} width={size}>
+            <GHIcon />
+          </Icon>
+        </a>
+        <a
+          href="https://www.github.com/peaceteddie/"
+          rel="noreferrer"
+          target="_blank"
+          title="Link to my LinkedIn"
+        >
           <Icon
-            viewBox="0 0 98 96"
+            viewBox="0 0 76.624 65.326"
+            filter={"contrast(0%) brightness(" + brightness + "%)"}
             height={size}
-            marginBlock={smaller}
-            padding={2}
+            px={2}
             width={size}
           >
-            <GHIcon />
+            <LinkedInIcon />
           </Icon>
         </a>
       </Center>
